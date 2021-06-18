f8bcf5fe611d9d6348082a8370b7c3da154512f3
renatocuellar <matealva@gmail.com>
2021-06-17 21:41:37 -0500

    comenzamos a hacer nuestro javascript

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const displayLastValue = document.getElementById(calculator__displayLastValue);
+const displayNewtValue = document.getElementById(calculator__displayNewValue);
+const numberButtons = document.querySelectorAll('.calculator__button-number');
+const operatorButtons = document.querySelectorAll('.calculator__button-operator');

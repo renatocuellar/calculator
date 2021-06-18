ba8d40a1d8ed90fa5847f45eec3f0f095945d4e6
renatocuellar <matealva@gmail.com>
2021-06-17 21:42:31 -0500

    comenzamos a hacer nuestro javascript

---
+++ js/Calculator.js
@@ -0,0 +1,23 @@
+class calculatorAddition {
+    addition(num1, num2){
+        return num1 + num2
+    }
+};
+
+class calculatorSubtraction {
+    subtraction(num1, num2){
+        return num1 - num2
+    }
+};
+
+class calculatorMultiplication {
+    multiplication(num1, num2){
+        return num1 * num2
+    }
+};
+
+class calculatorDivision {
+    division(num1, num2){
+        return num1 / num2
+    }
+};
